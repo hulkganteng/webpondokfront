e17532fb7dbb1e6ea2de7d3603699efa31358c7b
hulkganteng <bopupalukman@gmail.com>
2025-02-28 01:27:26 +0700

    api

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:3002/api";
+const API_BASE_URL = "http://ppassyafiiyahbungah.com:3002/api";
 
 // Ambil token dari localStorage
 const getAuthHeaders = () => {
