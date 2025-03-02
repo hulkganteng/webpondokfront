b3bf780beb9388c798d47f27bb0b1536720119c3
hulkganteng <bopupalukman@gmail.com>
2025-03-02 22:45:32 +0700

    perbhn15

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import ViteSitemap from 'vite-plugin-sitemap';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    ViteSitemap({
+      siteUrl: 'https://ppassyafiiyahbungah.com/',  // Ganti dengan URL situs Anda
+      changefreq: 'weekly',  // Frekuensi pembaruan sitemap
+      priority: 0.7,         // Prioritas halaman
+    }),
+  ],
   define: {
     global: {},
   },
-})
+});
